612b36611233f9cde55ff70bb31721e741e25713
Bruno Gonçalves <bpvgoncalves+git@gmail.com>
2014-04-17 23:39:02 +0200

    improve the comments, implements SetInverse and makes minor changes to output

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,18 @@
 
 
 #################################################################################
-##  This function implements the 'Cache Matrix' object and its interface. It   ##
-##  returns a 'list of functions' used as interface to the object.             ##
+## This function works on a OOP-like philosophy, implementing the CacheMatrix  ##
+## object and its outside interface.                                           ##
+## It heavily depends on R's lexical scoping rules allowing all the functions  ##
+## defined inside the 'calls' to access the same environment (the one in which ##
+## the function itself is defined) and therefore cross-manipulate the data.    ##
+##                                                                             ##
+## Inputs:                                                                     ##
+## x: (o) the matrix for which we want to compute the inverse                  ##
+##                                                                             ##
+## Output:                                                                     ##
+## Retruns a set of functions inside a 'list' object which can be used to      ##
+## access and manipulate the object's variables and envirnoment.               ##                                       ##
 #################################################################################
 makeCacheMatrix <- function(x = matrix()) {
       
@@ -37,9 +47,15 @@ makeCacheMatrix <- function(x = matrix()) {
             IsCached <<- TRUE  #I'm quite optimistic: solve() may fail for some reason!
             CacheData <<- solve(x)
       }
+
+      ### Sets Inverse Matrix from an outside input
+      SetInverse <- function(InvMatrix){
+            IsCached <<- TRUE  
+            CacheData <<- InvMatrix
+      } 
       
       ### Reads Inverse Matrix from Cache
-      ReadInverse <- function(){
+      GetInverse <- function(){
             CacheData
       }  
       
@@ -55,29 +71,37 @@ makeCacheMatrix <- function(x = matrix()) {
       list(SetMatrix = SetMatrix, 
            GetMatrix=GetMatrix, 
            CacheExists=CacheExists, 
-           ComputeInverse=ComputeInverse, 
-           ReadInverse=ReadInverse,
+           ComputeInverse=ComputeInverse,
+           SetInverse=SetInverse,
+           GetInverse=GetInverse,
            CleanCache=CleanCache)
 }
 
 
 #################################################################################
-## This function expects to receive a 'Cache Matrix' object as parameter 'x'   ##
-##  and prints: i) a string saying what is the data origin (ie, computed live  ##
-##  or read from cache), and ii) the Inverse Matrix for one initial matrix     ##
-## previously given to the 'Cache Matrix' via its 'SetMatrix' property.        ##
+## This function handles the 'Cache Matrix' object it receives as mandatory    ##
+## parameter and based on the existence of cached data it 'decides' if it has  ##
+## to compute the Inverse Matrix (IM) on the fly (and store it for later use)  ##
+## or if the IM can be read from cache avoiding recomputing on each time the   ##
+## IM is needed.                                                               ##
+##                                                                             ##
+## Inputs:                                                                     ##
+## x: (m) a "Cache Matrix" object                                              ##
+##                                                                             ##
+## Output:                                                                     ##
+## Retruns a 'matrix' inside a 'list' object which label indicates the data    ##
+## source (computed vs read).                                                  ##
 #################################################################################
 cacheSolve <- function(x, ...) {
 
       ##Look at the cache status on the Cache Matrix object
       if (x$CacheExists()) {
             ## Inverse matrix is in cache... Let's read it
-            print("Read")
-            print(x$ReadInverse())
+            out<-list(Read=x$GetInverse())
       } else {
             ## Inverse matrix is not in cache... Let's compute and store it
-            print("Compute")
-            print(x$ComputeInverse())
-      }      
+            out<-list(Computed=x$ComputeInverse())
+      }
+      out
 }
 
